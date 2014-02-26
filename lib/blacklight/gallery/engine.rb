e6954711157c03ccef16703610679a8c54eef91c
Chris Beer <cabeer@stanford.edu>
2014-02-26 14:31:44 -0800

    add the index metadata partial to the gallery partials by default, but style the dl as a regular dl, not a dl-horizontal

--- lib/blacklight/gallery/engine.rb
+++ lib/blacklight/gallery/engine.rb
@@ -3,7 +3,7 @@ require 'blacklight'
 module Blacklight
   module Gallery
     class Engine < Rails::Engine
-      Blacklight::Configuration.default_values[:view].gallery.partials = [:index_header]
+      Blacklight::Configuration.default_values[:view].gallery.partials = [:index_header, :index]
     end
   end
 end
