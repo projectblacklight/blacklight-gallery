f11d6f68cf791b4ba787ae610a3dddce423f7537
Chris Beer <cabeer@stanford.edu>
2014-11-17 14:47:46 -0800

    Integrate osd viewer

--- blacklight-gallery.gemspec
+++ blacklight-gallery.gemspec
@@ -20,6 +20,7 @@ Gem::Specification.new do |spec|
   spec.add_dependency "rails"
   spec.add_dependency "blacklight", "~> 5.0"
   spec.add_dependency "bootstrap-sass", "~> 3.0"
+  spec.add_dependency "openseadragon"
 
   spec.add_development_dependency "bundler", "~> 1.5"
   spec.add_development_dependency "rake"
--- lib/blacklight/gallery/engine.rb
+++ lib/blacklight/gallery/engine.rb
@@ -3,8 +3,8 @@ require 'blacklight'
 module Blacklight
   module Gallery
     class Engine < Rails::Engine
-      Blacklight::Configuration.default_values[:view].gallery.partials = [:index_header, :index]
-      Blacklight::Configuration.default_values[:view].slideshow.partials = [:index]
+      require 'openseadragon'
+    
     end
   end
 end
