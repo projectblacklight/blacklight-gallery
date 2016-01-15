3881295fd580949129c3418a545956508c0f56dc
Jessie Keck <jessie.keck@gmail.com>
2016-01-15 09:50:16 -0800

    Prevent SolrDocument#fetch from throwing an error if the document does not have the field.

--- app/models/concerns/blacklight/gallery/openseadragon_solr_document.rb
+++ app/models/concerns/blacklight/gallery/openseadragon_solr_document.rb
@@ -1,7 +1,7 @@
 module Blacklight::Gallery::OpenseadragonSolrDocument
-  def to_openseadragon view_config = nil
-    if view_config and view_config.tile_source_field
-      Array(fetch(view_config.tile_source_field))
-    end
+  def to_openseadragon(view_config = nil)
+    return unless view_config.try(:tile_source_field) &&
+                  fetch(view_config.tile_source_field, nil)
+    Array(fetch(view_config.tile_source_field))
   end
 end
