cbaeab34b662683c7710a49b648639c3f782f9e2
Chris Beer <cabeer@stanford.edu>
2020-10-14 11:21:19 -0700

    Remove support for format-specific partial rendering

--- app/helpers/blacklight/gallery_helper.rb
+++ app/helpers/blacklight/gallery_helper.rb
@@ -1,15 +1,5 @@
 module Blacklight
   module GalleryHelper
-    def render_gallery_collection documents
-      index = -1
-      documents.map do |object|
-        index += 1
-        template = gallery_wrapper_template(object)
-        template.render(self, {document: object, document_counter: index}) if template
-
-      end.join().html_safe
-    end
-
     def render_slideshow_tag(document, image_options = {}, url_options = {})
       if blacklight_config.view_config(document_index_view_type).slideshow_method
         method_name = blacklight_config.view_config(document_index_view_type).slideshow_method
@@ -26,16 +16,6 @@ module Blacklight
     def slideshow_image_url(document)
       if document.has? blacklight_config.view_config(document_index_view_type).slideshow_field
         document.first(blacklight_config.view_config(document_index_view_type).slideshow_field)
-      end
-    end
-
-    def gallery_wrapper_template(object)
-      format = document_partial_name(object, nil)
-      ['index_gallery_%{format}_wrapper', 'index_gallery'].each do |str|
-        partial = str % { format: format }
-        logger.debug "Looking for gallery document wrapper #{partial}"
-        template = lookup_context.find_all(partial, lookup_context.prefixes, true, [:document, :document_counter], {}).first
-        return template if template
       end
     end
   end
--- spec/helpers/blacklight/gallery_helper_spec.rb
+++ spec/helpers/blacklight/gallery_helper_spec.rb
@@ -4,30 +4,6 @@ describe Blacklight::GalleryHelper, :type => :helper do
   before do
     allow(helper).to receive(:blacklight_configuration_context).and_return(double(evaluate_if_unless_configuration: true))
   end
-  describe "#render_gallery_collection" do
-    let(:template) { double }
-    before do
-      allow(template).to receive(:render).and_return("hello ")
-      allow(helper).to receive(:gallery_wrapper_template).and_return(template)
-    end
-    let(:documents) { [ double, double] }
-    subject { helper.render_gallery_collection documents}
-
-    it { is_expected.to eq 'hello hello ' }
-  end
-
-  describe "#gallery_wrapper_template" do
-    before do
-      allow(helper).to receive(:blacklight_config).and_return(CatalogController.blacklight_config)
-      helper.lookup_context.prefixes << "catalog"
-    end
-
-    subject { helper.gallery_wrapper_template SolrDocument.new }
-
-    it "should be the default template" do
-       expect(subject.virtual_path).to eq 'catalog/_index_gallery'
-    end
-  end
 
   describe "render_slideshow_tag" do
     let(:document) { instance_double(SolrDocument) }
