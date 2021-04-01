f6a9e38a6dd2053218479330f201662da76fe9ad
Chris Beer <chris@cbeer.info>
2021-04-01 13:25:11 -0700

    Update masonry and use its gutters, not bootstrap's

--- app/assets/javascripts/blacklight_gallery/masonry.js
+++ app/assets/javascripts/blacklight_gallery/masonry.js
@@ -2,11 +2,13 @@
   $.fn.BlacklightMasonry = function() {
     var container = this;
     if(container.length > 0) {
-      container.imagesLoaded(function(){
-        container.masonry();
+      container.imagesLoaded().progress(function(){
+        container.masonry($.fn.BlacklightMasonry.options);
       });
     }
   }
+
+  $.fn.BlacklightMasonry.options = { gutter: 8 };
 })(jQuery);
 
 Blacklight.onLoad(function() {
