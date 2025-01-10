77ac96cd7104060d9b163c4d43cfc3b435bd097d
Justin Coyne <jcoyne@justincoyne.com>
2025-01-10 11:46:04 -0600

    Fix importmap path

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,4 +1,4 @@
 # frozen_string_literal: true
 
-pin_all_from File.expand_path('../app/javascripts/blacklight-gallery', __dir__), under: 'blacklight-gallery'
+pin_all_from File.expand_path('../app/javascript/blacklight-gallery', __dir__), under: 'blacklight-gallery'
 pin_all_from File.expand_path('../vendor/assets/javascripts', __dir__)
