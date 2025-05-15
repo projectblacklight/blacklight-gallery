309d81d8e1b12d2c8cc891bc818712a0ad989be6
Patrick Perkins <pperkins@upenn.edu>
2025-05-15 16:26:24 -0400

    just re-write the bootstrap classes in plain css - simpler and more maintainable that conditionally rendering classes on the caption area

--- app/components/blacklight/gallery/document_component.rb
+++ app/components/blacklight/gallery/document_component.rb
@@ -11,14 +11,6 @@ module Blacklight
       def render_document_class(*args)
         @view_context.render_document_class(*args)
       end
-
-      def caption_area_classes
-        if params[:view] == 'masonry'
-          %w[caption-area bg-dark text-white]
-        else
-          %w[caption-area]
-        end
-      end
     end
   end
 end
