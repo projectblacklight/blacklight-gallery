1323db6ce01771b2a02190f07f377eb338a9e6b9
Carla Galarza <cgalarza@upenn.edu>
2025-05-29 08:54:46 -0400

    Removing unused sprockets directives.

--- lib/generators/blacklight_gallery/install_generator.rb
+++ lib/generators/blacklight_gallery/install_generator.rb
@@ -84,19 +84,6 @@ module BlacklightGallery
       return unless defined?(Sprockets)
 
       append_to_file 'app/assets/config/manifest.js', "\n//= link blacklight_gallery/manifest.js\n"
-
-      insert_into_file "app/assets/javascripts/application.js", after: '//= require blacklight/blacklight' do
-        "\n//= require blacklight_gallery/blacklight-gallery"
-      end
-
-      insert_into_file "app/assets/javascripts/application.js" do
-        <<~CONTENT
-            Blacklight.onLoad(function() {
-              $('.documents-masonry').BlacklightMasonry();
-              $('.documents-slideshow').slideshow();
-            });
-          CONTENT
-      end
     end
   end
 end
