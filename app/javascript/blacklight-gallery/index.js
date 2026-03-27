dacfb280d3f91c6817cc3c630efd107a6d382320
Justin Coyne <jcoyne@justincoyne.com>
2026-03-27 11:28:43 -0500

    Use grid-lanes polyfill instead of masonry.js
    This allows us to remove jquery as a dependency

--- app/javascript/blacklight-gallery/index.js
+++ app/javascript/blacklight-gallery/index.js
@@ -1,1 +1,9 @@
 import "blacklight-gallery/slideshow"
+
+import { supportsGridLanes, init } from "blacklight-gallery/grid-lanes-polyfill"
+
+document.addEventListener("DOMContentLoaded", () => {
+  if (!supportsGridLanes()) {
+    init({ force: true })
+  }
+})
